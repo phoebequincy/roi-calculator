5f5d78be583465fb2d54529124f88a57dcfb8769
Phoebe Quincy <phoebequincy@gmail.com>
2019-05-30 12:17:09 -0600

    Initial file set up and install of styling dependencies.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { Container } from 'react-bootstrap'
 import './App.css';
+import Header from './components/Header.js';
+import InputForm from './components/InputForm.js';
+import Results from './components/Results.js';
+import DataTable from './components/DataTable.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+
+  render() {
+
+    return (
+      <div className="App">
+        <Container>
+          <Header />
+          <InputForm />
+          <Results />
+          <DataTable />
+        </Container>
+      </div>
+    );
+  }
 }
 
 export default App;
